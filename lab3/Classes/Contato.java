edb77e74700c0ba43fa561cf253240ec5e28d645
PedroCost4 <pedro.costacdc@gmail.com>
2022-10-26 09:55:07 -0300

    lcc

---
+++ lab3/Classes/Contato.java
@@ -0,0 +1,36 @@
+package Classes;
+
+public class Contato {
+    private String nome;
+    public String sobrenome;
+    public String telefone;
+
+
+    public Contato(String nome, String sobrenome, String telefone) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.telefone = telefone;
+    }
+
+    public Contato (String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getSobrenome() {
+        return this.sobrenome;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s\n%s", this.nome, this.sobrenome, this.telefone);
+    }
+} 
